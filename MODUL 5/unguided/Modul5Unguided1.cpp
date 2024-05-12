1b09f4ee87dc06093aad4a054bd68e5c61387b2a
Dsharlendita <163122642+Dsharlendita@users.noreply.github.com>
2024-05-13 00:57:08 +0700

    Add files via upload

---
+++ MODUL 5/unguided/Modul5Unguided1.cpp
@@ -0,0 +1,241 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+const int MAX_SIZE = 10;
+
+// struktur data untuk setiap node
+struct mahasiswa
+{
+    string nama;
+    long long NIM;
+    int nilai;
+    mahasiswa *next; // pointer untuk menunjukkan node selanjutnya dalam collision handling
+
+    mahasiswa(string nama, long long NIM, int nilai) : nama(nama), NIM(NIM), nilai(nilai), next(nullptr) {}
+};
+
+// class hash table
+class HashTable
+{
+private:
+    mahasiswa **table; // array pointer untuk menunjukkan ke elemen-elemen tabel hash
+
+    // fungsi hash sederhana
+    int hash_func(long long key)
+    {
+        return key % MAX_SIZE; // menggunakan modulus untuk mendapatkan indeks
+    }
+
+public:
+    // constructor
+    HashTable()
+    {
+        table = new mahasiswa *[MAX_SIZE](); // inisialisasi array pointer dengan nullptr
+    }
+
+    // destructor
+    ~HashTable()
+    {
+        // menghapus semua node dan array pointer
+        for (int i = 0; i < MAX_SIZE; ++i)
+        {
+            mahasiswa *current = table[i];
+            while (current != nullptr)
+            {
+                mahasiswa *temp = current;
+                current = current->next;
+                delete temp;
+            }
+        }
+        delete[] table;
+    }
+
+    // insertion
+    void insert(string nama, long long NIM, int nilai)
+    {
+        int index = hash_func(NIM);                                 // mendapatkan indeks berdasarkan NIM
+        mahasiswa *new_mahasiswa = new mahasiswa(nama, NIM, nilai); // membuat node baru
+        new_mahasiswa->next = table[index];                         // menambahkan node baru ke depan daftar di indeks yang tepat
+        table[index] = new_mahasiswa;
+    }
+
+    // deletion
+    void remove(long long NIM)
+    {
+        int index = hash_func(NIM);        // mendapatkan indeks berdasarkan NIM
+        mahasiswa *current = table[index]; // mengambil node pertama di indeks yang tepat
+        mahasiswa *prev = nullptr;         // pointer untuk node sebelumnya
+        while (current != nullptr)
+        {
+            if (current->NIM == NIM)
+            {
+                if (prev == nullptr)
+                {
+                    table[index] = current->next; // jika node yang dihapus adalah node pertama di indeks, atur node berikutnya sebagai node pertama
+                }
+                else
+                {
+                    prev->next = current->next; // jika bukan, hubungkan node sebelumnya dengan node setelahnya
+                }
+                delete current; // hapus node yang ditemukan
+                cout << "Mahasiswa dengan NIM " << NIM << " telah dihapus." << endl;
+                return;
+            }
+            prev = current;
+            current = current->next;
+        }
+        cout << "Mahasiswa dengan NIM " << NIM << " tidak ditemukan." << endl;
+    }
+
+    // searching by NIM
+    mahasiswa *cari_NIM(long long NIM)
+    {
+        long long index = hash_func(NIM);  // mendapatkan indeks berdasarkan NIM
+        mahasiswa *current = table[index]; // mengambil node pertama di indeks yang tepat
+        while (current != nullptr)
+        {
+            if (current->NIM == NIM)
+            {
+                return current; // kembalikan node jika NIM cocok
+            }
+            current = current->next;
+        }
+        return nullptr; // kembalikan nullptr jika tidak ditemukan
+    }
+
+    // searching by rentang nilai
+    void cari_nilai(int awal, int akhir)
+    {
+        // mencetak header tabel
+        cout << "-----------------------------------------------------------------------" << endl;
+        cout << "|         Nama Mahasiswa          |         NIM          |    Nilai   |" << endl;
+        cout << "-----------------------------------------------------------------------" << endl;
+        // traverse semua bucket pada tabel hash
+        for (int i = 0; i < MAX_SIZE; ++i) {
+            mahasiswa* current = table[i]; // mengambil node pertama di indeks yang tepat
+                    // traverse semua node dalam bucket
+            while (current != nullptr) {
+                // jika nilai berada dalam rentang yang ditentukan, cetak informasi mahasiswa
+                if (current->nilai >= awal && current->nilai <= akhir) {
+                    string namaSingkat = current->nama.substr(0, 30);
+                    cout << "| " << setw(30) << left << namaSingkat << "  | ";
+                    cout << setw(20) << current->NIM << " | ";
+                    cout << setw(10) << fixed << setprecision(2) << current->nilai << " |" << endl;
+                }
+            current = current->next;
+            }
+        }
+        cout << "-----------------------------------------------------------------------" << endl;
+    }
+
+    // traversal
+   void traverse() {
+    cout << "-----------------------------------------------------------------------" << endl;
+    cout << "|         Nama Mahasiswa          |         NIM          |    Nilai   |" << endl;
+    cout << "-----------------------------------------------------------------------" << endl;
+
+    for (int i = 0; i < MAX_SIZE; ++i) {
+        mahasiswa* current = table[i];
+
+        while (current != nullptr) {
+        string namaSingkat = current->nama.substr(0, 30); // Memotong 30 karakter pertama
+        cout << "| " << setw(30) << left << namaSingkat << "  | ";
+        cout << setw(20) << current->NIM << " | ";
+        cout << setw(10) << current->nilai << " |" << endl;
+
+        current = current->next;
+        }
+    }
+
+    cout << "-----------------------------------------------------------------------" << endl;
+    }
+};
+
+int main()
+{
+    HashTable ht;
+
+    int pilih, nilai, awal, akhir;
+    string nama;
+    long long NIM;
+
+    do
+    {
+        // menu utama
+        cout << endl;
+        cout << " M E N U : " << endl << endl;
+        cout << "-----------------------------------------------------------------------" << endl << endl;
+        cout << "1. Tambah Data Mahasiswa" << endl;
+        cout << "2. Hapus Data Mahasiswa" << endl;
+        cout << "3. Cari berdasarkan NIM" << endl;
+        cout << "4. Cari berdasarkan Rentang Nilai" << endl;
+        cout << "5. Tampilkan Semua Data" << endl;
+        cout << "6. Keluar" << endl << endl;
+        cout << "-----------------------------------------------------------------------" << endl << endl;
+        cout << "Pilih menu : ";
+        cin >> pilih;
+        cout << endl;
+
+        switch (pilih) {
+        case 1:
+            // tambah data mahasiswa
+            cout << "Masukan nama : ";
+            cin.ignore();
+            getline(cin, nama);
+            cout << "Masukan NIM : ";
+            cin >> NIM;
+            cout << "Masukan nilai : ";
+            cin >> nilai;
+            ht.insert(nama, NIM, nilai);
+            cout<<"Data berhasil ditambahkan"<<endl;
+            cout << "-----------------------------------------------------------------------" << endl << endl;
+            break;
+        case 2:
+            // hapus data mahasiswa berdasarkan NIM
+            cout << "Masukan NIM yang ingin dihapus : ";
+            cin >> NIM;
+            ht.remove(NIM);
+            cout << "------------------------------------------------------------------------" << endl << endl;
+            break;
+        case 3:
+            // cari mahasiswa berdasarkan NIM
+            cout << "Masukan NIM yang ingin dicari : ";
+            cin >> NIM;
+            {
+                mahasiswa *mahasiswa_ptr = ht.cari_NIM(NIM);
+                if (mahasiswa_ptr)
+                {
+                    cout << "Ditemukan mahasiswa dengan NIM " << NIM << " bernama " << mahasiswa_ptr->nama << " dan memiliki nilai " << mahasiswa_ptr->nilai << endl;
+                }
+                else
+                {
+                    cout << "mahasiswa dengan NIM " << NIM << " tidak ditemukan" << endl;
+                }
+            }
+            cout << "------------------------------------------------------------------------" << endl << endl;
+            break;
+        case 4:
+            // cari mahasiswa berdasarkan rentang nilai
+            cout << "masukan nilai awal : ";
+            cin >> awal;
+            cout << "masukan nilai akhir : ";
+            cin >> akhir;
+            ht.cari_nilai(awal, akhir);
+            break;
+        case 5:
+            // tampilkan semua data mahasiswa
+            ht.traverse();
+            break;
+        case 6:
+            cout << "Terimakasih" << endl; // keluar dari program
+            cout << "-------------------------------------------------------------------------" << endl << endl;
+            break;
+        default:
+            cout << "Pilihan anda tidak tersedia!" << endl; // jika pilihan tidak valid
+            cout << "-------------------------------------------------------------------------" << endl << endl;
+        }
+    } while (pilih != 6); // ulangi sampai pengguna memilih untuk keluar
+
+    return 0;
+}
